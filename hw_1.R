b4623a25d816e7e0f2ce550a6990b8b6a003d034
Philip Anderson <panders225@gmail.com>
2017-09-04 09:42:13 -0400

    finally completed mv-normal density plotting

--- hw_1.R
+++ hw_1.R
@@ -32,8 +32,7 @@ oxy_summ
 # on average, higher oxygen volumes in all scenarios, but higher
 # variance on these metrics while conducting strenuous exercise
 
-
-# B - make a pairs plot (pairwise scatterplot)
+ # B - make a pairs plot (pairwise scatterplot)
 
 pairs(oxygen)
 # replaces one half of symmetrical matrix with other info
@@ -64,105 +63,63 @@ coplot(V1 ~ V3 | V5, columns=2
 
 # B - use the persp function to graph the pdf
 
-library(MASS)
-mu <- c(1,-1)
-sigma <- matrix(c(1.0, -1.6, -1.6, 4.0), 2)
-bivn <- mvrnorm(n=5000, mu=mu, Sigma=sigma)
-plot(density(bivn[,1]))
-plot(density(bivn[,2]))
-
-bivn.kde <- kde2d(
-                  x=bivn[,1]
-                  , y=bivn[,2]
-                  , n=50
-                  )
-
-
-persp(bivn.kde, phi=20, theta=35, ticktype = 'detailed'
-      ,xlab="x coordinate"
-      , ylab="y coordinate")
-
-help(dmvnorm)
-matrix(-10:9, nrow=10)
-
-fill <- matrix(0, nrow=100, ncol=100)
-
-base_df <- data.frame(i_val=0
-           ,j_val=0
-           , density_val=0)
-
-for (i in seq(-5,5, by=0.1))
-  {
-   for (j in seq(-5,5, by=0.1))
-   {
-   hold <- dmvnorm(
-                   x=c(i,j)
-                  , mean=c(1,-1)
-                  , sigma=matrix(c(1.0, -1.6, -1.6, 4.0), nrow=2 , byrow=T)
-                   )
-   iter_df <- data.frame(
-                         i_val=i
-                         , j_val=j
-                         , density_val=hold
-                         )
-   base_df <- rbind(base_df, iter_df)
-   }
-  }
-
-base_df <- base_df[-1,]
-
-den_val <- as.matrix(base_df['density_val'])
-head(den_val)
-
-persp(
-  x=seq(-5,5,by=0.1)
-  , y=seq(-5,5,by=0.1)
-  , z=den_val
-  )
 
 
 
 # specify a function
+# im going to be hard-coding the mean and covariance matrices
 
-phil_mvnorm <- function(mu_vec, sigma_mat, p=2, x_input)
+phil_mvnorm <- function(x, y)
   {
-term_one <- (1 / ( ( (2*pi)**(p/2) ) * ( (det(sigma_mat))**(1/2) ) ) ) 
-
-term_two <- exp(((-t(x_input - mu_vec)) %*% (solve(sigma_mat)) %*% (x_input - mu_vec)) / 2)
-
-term_three <- term_one * term_two
-
-return(term_three)
+    x_input <- matrix(c(x,y), nrow=2)
+    mu_vec <- matrix(c(1 ,-1), nrow=2)
+    sigma_mat <- matrix(c(1.0, -1.6, -1.6, 4.0), nrow=2 , byrow=T)
+    p <- 2
+    
+    term_one <- (1 / ( ( (2*pi)**(p/2) ) * ( (det(sigma_mat))**(1/2) ) ) ) 
+    
+    term_two <- exp(((-t(x_input - mu_vec)) %*% (solve(sigma_mat)) %*% (x_input - mu_vec)) / 2)
+    
+    term_three <- term_one * term_two
+    
+    return(term_three)
   }
 
+# set up a loop to create a 'long' dataset
 
-fill <- matrix(0, nrow=100, ncol=100)
+base_df <- data.frame(x_val=0
+                      , y_val=0
+                      , z_val=0)
 
-# x
-for (i in seq(-2,4,length.out=100))
-{
-  # y 
-  for (j in seq(-10,5,length.out=100))
+for (i in seq(-4,4,length.out=50))
   {
-  fill[i,j] <- phil_mvnorm(
-                           mu_vec=matrix(c(1 ,-1), nrow=2)
-                           , matrix(c(1.0, -1.6, -1.6, 4.0), nrow=2 , byrow=T)
-                           , x_input=matrix(c(i,j), nrow=2)
+    for (j in seq(-4,4, length.out=50))
+    {
+      iter_df <- data.frame(x_val=i
+                            , y_val=j
+                            , z_val=phil_mvnorm(x=i, y=j)
                             )
+      base_df <- rbind(base_df, iter_df)
+    }
   }
-}
 
-nrow(fill)
+base_df <- base_df[-1,]
 
-persp(x=seq(-2, 4, length.out=100)
-      , y=seq(-10,5, length.out=100)
-      , z=fill
-      , ticktype="detailed"
-      , xlab='x-axis'
-      , ylab='y-axis')
+final_grid <- reshape::cast(base_df, x_val ~ y_val
+                            , fun.aggregate=mean
+                            ) 
 
-View(fill)
-help(persp)
+
+final_grid <- final_grid[,-1]
+final_grid <- data.frame(final_grid)
+final_grid <- as.matrix(final_grid)
+
+x <- y <- seq(-4, 4, length.out=50)
+persp(x, y, final_grid, phi=30, theta=60
+      , ticktype='detailed'
+      , main="Multivariate Density")
+
+
 
 # question 4
 
@@ -187,3 +144,47 @@ two_inv
 
 
 # question 6
+
+library("plotrix")
+
+plot(c(0,10), c(0,10), type="n", main="test draw.ellipse")
+draw.ellipse(c(3,7), c(8,8), c(5,1), c(1,5), col=c(2,4),
+             angle=c(45,0), segment=rbind(c(0,45),c(45,360)))
+
+draw.ellipse(c(7,7), c(7,8), c(0.5,1), c(1,0.5), col=c(2,4),
+             angle=c(45,0), segment=rbind(c(0,45),c(45,360)))
+draw.ellipse(c(7,7), c(7,8), c(0.5,1), c(1,0.5), col=c(2,4),
+             angle=c(45,0), segment=rbind(c(0,45),c(45,360)))
+
+help("draw.ellipse")
+
+# 95th percentile of the chi squared distribution
+qchisq(p=0.95, df=2)
+sigma1 <- matrix(c(1.00, 0.80, 0.80, 1.00), nrow=2, byrow=F)
+solve(sigma1)
+help("draw.ellipse")
+
+plot(c(-5,5), c(-5,5), type="n")
+lines(mv_one[,1], mv_one[,2])
+
+mean(mv_one[,1])
+mean(mv_one[,2])
+
+sigma1
+mv_one <- rmvnorm(n = 5000, mean=c(1,1), sigma=sigma1)
+
+head(mv_one)
+
+bivn.kde <- kde2d(
+                  x=mv_one[,1]
+                  , y=mv_one[,2]
+                  , n=50
+                  )
+
+persp(bivn.kde, phi=20, theta=35, ticktype = 'detailed'
+      ,xlab="x coordinate"
+      , ylab="y coordinate")
+
+
+
+draw.ellipse()
