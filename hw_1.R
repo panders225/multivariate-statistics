9a24682695207bac694e2b460d13ae43b63c6360
Philip Anderson <panders225@gmail.com>
2017-09-12 19:07:10 -0400

    finished hw01

--- hw_1.R
+++ hw_1.R
@@ -26,7 +26,7 @@ oxy_summ <- oxygen %>%
                    , X4_sd=sd(V4)
                    )
 
-oxy_summ
+View(oxy_summ)
 
 # without conducting any tests, it appears that men have, 
 # on average, higher oxygen volumes in all scenarios, but higher
@@ -34,14 +34,14 @@ oxy_summ
 
  # B - make a pairs plot (pairwise scatterplot)
 
-pairs(oxygen)
+pairs(oxygen, main="Pairs Plot for Oxygen Data")
 # replaces one half of symmetrical matrix with other info
 GGally::ggpairs(oxygen)
 
 # one individual appears to have very high values for 
 # V1 and V2 - let's determine who that is
 summary(oxygen)
-
+max(oxygen$V1)
 subset(oxygen, V1 >= 0.65)
 
 # record 48 appears to be an outlier
@@ -63,21 +63,19 @@ coplot(V1 ~ V3 | V5, columns=2
 
 # B - use the persp function to graph the pdf
 
-
-
-
 # specify a function
-# im going to be hard-coding the mean and covariance matrices
 
-phil_mvnorm <- function(x, y)
+phil_mvnorm <- function(x, y
+                        , mu_vec=matrix(c(1 ,-1), nrow=2)
+                        , sigma_mat=matrix(c(1.0, -1.6, -1.6, 4.0), nrow=2 , byrow=T)
+                        , p=2
+                        )
   {
+    # bring the two x inputs into a column vector
     x_input <- matrix(c(x,y), nrow=2)
-    mu_vec <- matrix(c(1 ,-1), nrow=2)
-    sigma_mat <- matrix(c(1.0, -1.6, -1.6, 4.0), nrow=2 , byrow=T)
-    p <- 2
-    
+    # specify the constant term
     term_one <- (1 / ( ( (2*pi)**(p/2) ) * ( (det(sigma_mat))**(1/2) ) ) ) 
-    
+    # specify the matrix piece of things
     term_two <- exp(((-t(x_input - mu_vec)) %*% (solve(sigma_mat)) %*% (x_input - mu_vec)) / 2)
     
     term_three <- term_one * term_two
@@ -86,11 +84,11 @@ phil_mvnorm <- function(x, y)
   }
 
 # set up a loop to create a 'long' dataset
-
+# results collector up front
 base_df <- data.frame(x_val=0
                       , y_val=0
                       , z_val=0)
-
+# this will take us through a 50x50 grid moving from -4 to 4
 for (i in seq(-4,4,length.out=50))
   {
     for (j in seq(-4,4, length.out=50))
@@ -103,11 +101,15 @@ for (i in seq(-4,4,length.out=50))
     }
   }
 
+# drop off the original record as we don't need it
 base_df <- base_df[-1,]
 
 final_grid <- reshape::cast(base_df, x_val ~ y_val
                             , fun.aggregate=mean
                             ) 
+
+head(base_df)
+head(final_grid)
 
 
 final_grid <- final_grid[,-1]
@@ -119,6 +121,10 @@ persp(x, y, final_grid, phi=30, theta=60
       , ticktype='detailed'
       , main="Multivariate Density")
 
+t(matrix(c(-1,1), nrow=2)) %*%
+  matrix(c(2.77, 1.11, 1.11, 0.69), nrow=2) %*% 
+  matrix(c(-1,1), nrow=2) %>%
+  sqrt()
 
 
 # question 4
@@ -141,53 +147,6 @@ one_inv
 two_inv
 
 3*two_inv
-
-
-# question 6
-
-library("plotrix")
-
-plot(c(0,10), c(0,10), type="n", main="test draw.ellipse")
-draw.ellipse(c(3,7), c(8,8), c(5,1), c(1,5), col=c(2,4),
-             angle=c(45,0), segment=rbind(c(0,45),c(45,360)))
-
-draw.ellipse(c(7,7), c(7,8), c(0.5,1), c(1,0.5), col=c(2,4),
-             angle=c(45,0), segment=rbind(c(0,45),c(45,360)))
-draw.ellipse(c(7,7), c(7,8), c(0.5,1), c(1,0.5), col=c(2,4),
-             angle=c(45,0), segment=rbind(c(0,45),c(45,360)))
-
-help("draw.ellipse")
-
-# 95th percentile of the chi squared distribution
-qchisq(p=0.95, df=2)
-sigma1 <- matrix(c(1.00, 0.80, 0.80, 1.00), nrow=2, byrow=F)
-solve(sigma1)
-help("draw.ellipse")
-
-plot(c(-5,5), c(-5,5), type="n")
-lines(mv_one[,1], mv_one[,2])
-
-mean(mv_one[,1])
-mean(mv_one[,2])
-
-sigma1
-mv_one <- rmvnorm(n = 5000, mean=c(1,1), sigma=sigma1)
-
-head(mv_one)
-
-bivn.kde <- kde2d(
-                  x=mv_one[,1]
-                  , y=mv_one[,2]
-                  , n=50
-                  )
-
-persp(bivn.kde, phi=20, theta=35, ticktype = 'detailed'
-      ,xlab="x coordinate"
-      , ylab="y coordinate")
-
-
-
-draw.ellipse()
 
 # question 6
 
